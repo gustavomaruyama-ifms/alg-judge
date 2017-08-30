11e4d4ca5c89fbcdacd8dc8b155869e2958d87af
= <=>
2017-08-29 22:50:36 -0400

    Exemplo de utilizacao do interpretador

--- src/main/java/br/ifms/cx/algjudge/rest/SubmissaoResource.java
+++ src/main/java/br/ifms/cx/algjudge/rest/SubmissaoResource.java
@@ -5,11 +5,18 @@
  */
 package br.ifms.cx.algjudge.rest;
 
+import br.com.vinyanalista.portugol.interpretador.Interpretador;
+import br.com.vinyanalista.portugol.interpretador.Terminal;
+import br.ifms.cx.algjudge.domain.Response;
+import br.ifms.cx.algjudge.domain.Submissao;
+import java.awt.Color;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -20,5 +27,16 @@ import org.springframework.stereotype.Component;
 @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
 @Component
 public class SubmissaoResource {
-    
+    @POST
+    @Transactional
+    public Response submeter(Submissao submissao) {
+        try {
+            //InterpretadorObject i = new InterpretadorObject(submissao);
+            //i.execute();
+            //dao.persistirSubmissao(submissao);            
+            return Response.Ok("Submissao incluida com sucesso");
+        } catch (Exception ex) {
+            return Response.Error(ex.getMessage());
+        }
+    }
 }
---
+++ src/test/java/br/ifms/cx/algjudge/teste/MainTeste.java
@@ -0,0 +1,56 @@
+package br.ifms.cx.algjudge.teste;
+
+import java.awt.Color;
+
+import br.com.vinyanalista.portugol.interpretador.Exemplo;
+import br.com.vinyanalista.portugol.interpretador.Interpretador;
+import br.com.vinyanalista.portugol.interpretador.Terminal;
+
+public class MainTeste {
+	public static void main(String[] args) {
+		String codigoFonte = Exemplo.ESTRUTURA_CONDICIONAL_SIMPLES.getProgramaFonte();
+
+		System.out.println(codigoFonte);
+		Interpretador interpretador = new Interpretador(new Terminal() {
+			String[] entradas = {"50","20"};
+			Integer seqEntrada = 0;
+
+			@Override
+			protected void mudarCor(Color cor) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void limpar() {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			protected String ler() {
+				return entradas[seqEntrada++];
+			}
+
+			@Override
+			public void informacao(String mensagemDeInformacao) {
+				System.out.println(mensagemDeInformacao);
+			}
+
+			@Override
+			protected void escrever(String mensagem) {
+				System.out.println(mensagem);
+			}
+
+			@Override
+			public void erro(String mensagemDeErro) {
+				System.out.println(mensagemDeErro);
+			}
+		});
+
+		try {
+			interpretador.analisar(codigoFonte);
+			interpretador.executar();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
